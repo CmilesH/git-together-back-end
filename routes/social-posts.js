2d517292ba5404ff418b374aacc4090bcb0cdbe6
Catherine Tung <catt.tung@gmail.com>
2022-03-27 17:40:46 -0400

    Add author to post

--- routes/social-posts.js
+++ routes/social-posts.js
@@ -1,17 +1,21 @@
 import { Router } from "express";
 import * as postsCtrl from '../controllers/social-posts.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 
 const router = Router()
+
+router.use(decodeUserFromToken)
+
 //Routes
-//localhost:3000/socialFeed
-router.get('/', postsCtrl.index)
-//localhost:3000/socialFeed
-router.post('/', postsCtrl.create)
-//localhost:3000/socialFeed/:id
+//localhost:3001/socialFeed
+router.get('/', checkAuth, postsCtrl.index)
+//localhost:3001/socialFeed
+router.post('/', checkAuth, postsCtrl.create)
+//localhost:3001/socialFeed/:id
 router.get('/:id', postsCtrl.show)
-//localhost:3000/socialFeed/:id
+//localhost:3001/socialFeed/:id
 router.put('/:id', postsCtrl.update)
-//localhost:3000/socialFeed/:id
+//localhost:3001/socialFeed/:id
 router.delete('/:id', postsCtrl.delete)
 
 export {
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
-app.use('/socialFeed', socialPostsRouter)
+app.use('/api/socialFeed', socialPostsRouter)
 
 app.use(function (req, res, next) {
   res.status(404).json({ err: "Not found" })
