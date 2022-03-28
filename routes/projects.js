bd4dbc5479b57f7878bce6985c172a47ace9da68
Catherine Tung <catt.tung@gmail.com>
2022-03-28 11:11:57 -0400

    Stub up files

--- routes/projects.js
+++ routes/projects.js
@@ -1,2 +1,12 @@
 import { Router } from 'express'
 import * as projectsCtrl from '../controllers/projects.js'
+
+const router = Router()
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+
+
+export { router }
